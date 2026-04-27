d5e8a300fe8463b8491c6c3e8bbf10362175ca48
zhoue <erwin.zhou26@gmail.com>
2026-04-27 11:41:42 +0200

    moved pydantic data types to seperate file and added the pyproject.toml file

--- server/src/tiphone_server/main.py
+++ server/src/tiphone_server/main.py
@@ -1,40 +1,13 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from pydantic import BaseModel
-from .websocketsManager import WebSocketConnectionManager
 
+
+from .websocketsManager import WebSocketConnectionManager
+from .dataTypes import AdvertisementMsg, AdvertisementResponse
 app = FastAPI()
 
 ws_manager = WebSocketConnectionManager()
 
 all_ids = []
-
-class AdvertisementMsg(BaseModel):
-    """
-    Client sends this message when powered on, to indicate that they are reachable
-    shared secret to ensure that client is part of the network. This secret is individual for each
-    client
-    """
-    secret: str
-    name: str
-
-class AdvertisementResponse(BaseModel):
-    """
-    Response message to the Advertisement with phone number
-    """
-    phone_number: str
-
-class SDP(BaseModel):
-    """
-    Session Description Protocol
-    """
-    # TODO: rework if necessary
-    v: str
-    o: str
-    s: str
-    c: str
-    t: str
-    m: str
-    a: str
     
 
 @app.get("/")
